42b39de942d24029a0999bf8793a072956c58df5
Tinotenda Chemvura <ttchemvura@gmail.com>
2017-12-16 15:57:38 +0200

    added email backend

--- InvoiceNow/settings.py
+++ InvoiceNow/settings.py
@@ -137,4 +137,5 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', True)
 EMAIL_ORIGIN = os.environ.get('EMAIL_ORIGIN', 'tino@tino.com')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 I_HOST = os.environ.get('I_HOST', '0.0.0.0:8000')
