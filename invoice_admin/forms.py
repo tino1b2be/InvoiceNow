4e5074093f3e45f9f9be7488ae02802fed4dec74
Tinotenda Chemvura <ttchemvura@gmail.com>
2017-12-17 14:41:55 +0200

    Updated chage client form

--- invoice_admin/forms.py
+++ invoice_admin/forms.py
@@ -4,6 +4,8 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit, Field
 from crispy_forms.bootstrap import FormActions
 
+from invoice_client.models import Client
+
 
 class NewClientForm(forms.Form):
     client_name = forms.CharField(max_length=50)
@@ -23,14 +25,19 @@ class NewClientForm(forms.Form):
 
 
 class ChargeClientForm(forms.Form):
-    client_id = forms.IntegerField(min_value=1)
+    names = Client.objects.values_list('user__first_name')
+    client_names = []
+    for name in names:
+        client_names.append((name[0], name[0]))
+
+    client_name = forms.ChoiceField(choices=client_names)
     description = forms.CharField(label='Work Description')
     fee = forms.FloatField(help_text='NB: fee in USD', min_value=0)
 
     helper = FormHelper()
     helper.form_class = 'form-horizontal'
     helper.layout = Layout(
-        Field('client_id', css_class='input-xlarge form-control'),
+        Field('client_name', css_class='input-xlarge form-control'),
         Field('description', css_class='form-control'),
         Field('fee', css_class='form-control'),
         FormActions(
